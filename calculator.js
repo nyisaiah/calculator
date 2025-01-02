75df9ba51bbbd01eb9f52c220baae7abcd4cee2f
Nyisaiah Hall <nyisaiah.hall@gmail.com>
2025-01-02 07:46:23 -0500

    Calculations work

--- calculator.js
+++ calculator.js
@@ -29,8 +29,26 @@ function switchNumber(){
         firstNumber.active = true
     }
 }
+function displayCalculation(number){
+    if(number.length > 11 && (Number(number) >= 1e11 || Number(number) <=1e-11)) display.textContent = String(Number(number).toExponential(2)).substring(0,11);
+    else display.textContent = number.substring(0,11);
+}
+function operate(a, b, operand){
+    switch (operand){
+        case "+":
+            return String(add(a,b));
+        case "-":
+            return String(subtract(a,b));
+        case "*":
+            return String(multiply(a,b));
+        case "/":
+            return String(divide(a,b));
+    }
+    
+}
 
 let firstNumber = {
+    name: "first",
     value: "0",
     active: true,
     decimal: false,
@@ -38,6 +56,7 @@ let firstNumber = {
 }
 
 let secondNumber = {
+    name: "second",
     value: "0",
     active: false,
     decimal: false,
@@ -45,6 +64,7 @@ let secondNumber = {
 }
 
 let currentOperator = ""
+let lastButtonNumber = false
 const numberPad = document.querySelector(`#number-pad`)
 const display = document.querySelector("#display")
 const operators = document.querySelectorAll(".operator")
@@ -56,25 +76,86 @@ numberPad.addEventListener('click', (event) => {
     if (currentNumber.value == "0" && target.value != undefined) {
         if(target.value == "."){
             currentNumber.value += target.value;
-            display.textContent += target.value;
+            displayCalculation(currentNumber.value)
             currentNumber.decimal = true
         }
         else {
             currentNumber.value = target.value;
-            display.textContent = target.value;
+            displayCalculation(currentNumber.value)
         }
     }
     else if (target.value == "." && currentNumber.decimal){
         return
     }
     else if (target.value == "." && !currentNumber.decimal){
-        display.textContent += target.value;
         currentNumber.value += target.value;
+        displayCalculation(currentNumber.value)
         currentNumber.decimal = true;
     }
     else if(target.value != undefined && target){
-        display.textContent += target.value;
         currentNumber.value += target.value;
+        displayCalculation(currentNumber.value)
     }
     console.table(currentNumber)
+    lastButtonNumber = true;
+    console.log(lastButtonNumber)
+})
+
+operators.forEach(function(operator) {
+    operator.addEventListener("click", ()=> {
+        let currentNumber = checkCurrentNumber()
+        
+        if (currentNumber == firstNumber){
+            console.log("working with the first number")
+            if(operator.value == "="){
+                console.log(operator.value + " was clicked");
+            }
+            else {
+                currentOperator = operator.value;
+                switchNumber()
+                console.log(currentOperator)
+                
+            }
+        }
+        else {
+            console.log("working with the second number")
+            if(!lastButtonNumber && operator.value != "="){
+                currentOperator = operator.value;
+                console.log("this case reached")
+            }
+            else if(operator.value == "="){
+                let result = operate(Number(firstNumber.value), Number(secondNumber.value), currentOperator)
+                console.log(result)
+                displayCalculation(result)
+                firstNumber.value = result;
+                if(Number(firstNumber < 0)){
+                    firstNumber.negative = true;
+                }
+                secondNumber.value = "0";
+                switchNumber();
+            }
+            else {
+                let result = operate(Number(firstNumber.value), Number(secondNumber.value), currentOperator)
+                console.log(result)
+                displayCalculation(result)
+                firstNumber.value = result;
+                if(Number(firstNumber < 0)){
+                    firstNumber.negative = true;
+                }
+                secondNumber.value = "0";
+                currentOperator = operator.value;
+
+            }
+        }
+
+    
+        if(operator.value == "="){
+            console.log(operator.value + " was clicked");
+        }
+        else {
+            console.log(currentNumber.value)
+        }
+        lastButtonNumber = false;
+        console.log(lastButtonNumber)
+    })
 })
